f9255f67d91eab8932eb4dce663762fd289d843b
GuyNamedAnanya <125535621+GuyNamedAnanya@users.noreply.github.com>
2023-06-26 15:34:16 +0530

    Backspace Works
    Backspace button works

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -12,6 +12,8 @@ public class WordManager : MonoBehaviour
     [SerializeField] List<Transform> wordBoxes = new List<Transform>();
 
     int currentWordBox;
+    int currentRow;
+    int countOfLettersPerRow = 4;
 
     List<string> dictionary = new List<string>();
     List<string> words = new List<string>();
@@ -48,8 +50,37 @@ public class WordManager : MonoBehaviour
     }
 
     public void LetterInBox(string letter)
+    {       
+
+        if (wordBoxes[(currentRow * countOfLettersPerRow) + currentWordBox].GetChild(0).GetComponent<TMP_Text>().text == "")
+        {
+            wordBoxes[(currentRow * countOfLettersPerRow) + currentWordBox].GetChild(0).GetComponent<TMP_Text>().text = letter;
+        }
+
+        if((currentRow * countOfLettersPerRow) + currentWordBox < (currentRow * countOfLettersPerRow) + countOfLettersPerRow)
+        {
+            currentWordBox++;
+        }
+    }
+
+    public void RemoveLetter()
     {
-        wordBoxes[currentWordBox].GetChild(0).GetComponent<TMP_Text>().text = letter;
-        currentWordBox++;
+        int selectedWordbox = (currentRow * countOfLettersPerRow) + currentWordBox;
+
+        if (wordBoxes[selectedWordbox].GetChild(0).GetComponent<TMP_Text>().text == "")
+        {
+            if(selectedWordbox > (currentRow * countOfLettersPerRow))
+            {
+                currentWordBox--;
+            }
+
+            selectedWordbox = (currentRow * countOfLettersPerRow) + currentWordBox;
+            wordBoxes[selectedWordbox].GetChild(0).GetComponent<TMP_Text>().text = "";
+        }
+
+        else
+        {
+            wordBoxes[selectedWordbox].GetChild(0).GetComponent<TMP_Text>().text = "";
+        }
     }
 }
